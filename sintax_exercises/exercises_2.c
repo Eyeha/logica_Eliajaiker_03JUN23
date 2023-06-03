d58adba27de8627ccec6c88fbb84b9c3c823b17c
Eyeha <eliajaikermolina4@gmail.com>
2023-06-03 17:13:41 -0400

    primer commit

---
+++ sintax_exercises/exercises_2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+    int main(){
+    printf("Bienvenidos Mundo");
+    printf("Estoy aprendiendo lenguaje C");
+    return 0;
+    }
+
+
+    /*
+    #include <stdio.h>
+    
+    es una libreria de archivos de header que nos permite trabajar con funciones de entrada y salida,como printf()(que se encuentra en la linea 4) los archivos de encabezado agreagan funcionalidad a los porgramas C.
+    */
---
+++ sintax_exercises/exercises_3.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main(){
+    /*
+    printf("Bienvenidos Mundo!\n");
+    printf("Estoy aprendiendo lenguaje C");
+    */
+    
+    /*
+    printf("Bienvenidos Mundo!\nEstoy aprendiendo C\nY es asombroso");
+    */ 
+
+    /*   
+    printf("Bienvenidos Mundo!\n\n");
+    printf("Estoy aprendiendo lenguaje C");
+    */
+
+    /*
+    printf("Bienvenidos Mundo!\t");
+    printf("Estoy aprendiendo lenguaje C");
+    */
+
+    /*
+    printf("Bienvenidos Mundo!\\");
+    printf("Estoy aprendiendo lenguaje C");
+    */
+     
+    printf("Ellos lo llaman \"Carlos\"");
+
+
+    
+    return 0;
+}
+
+
+    /*
+    #include <stdio.h>
+    
+    es una libreria de archivos de header que nos permite trabajar con funciones de entrada y salida,como printf()(que se encuentra en la linea 4) los archivos de encabezado agreagan funcionalidad a los porgramas C.
+    */
